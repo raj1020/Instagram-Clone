a1d84a4b65c470c3d9b74310e80afa92ef7503f0
raj1020 <raj.1020@hotmail.com>
2020-08-26 08:27:23 +1000

    started backend for Instagram Clone

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = 5000;
+const {MONGOURI} = require('./keys.gitignore');
+
+require('./models/user');
+
+mongoose.connect(MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log("Connected to MongoDB");
+})
+mongoose.connection.on('error', (err) => {
+    console.log("error connecting", err);
+})
+
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT ", PORT);
+});
+
+
+
+

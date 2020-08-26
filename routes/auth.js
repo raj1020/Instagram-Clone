c9b10b0714ec14f32d625758d9e8ce6d7a3df4a4
raj1020 <raj.1020@hotmail.com>
2020-08-27 09:11:46 +1000

    made changes in 'auth.js' to save data in mongodb

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = mongoose.model("User");
 
 
 router.get('/', (req, res) => {
@@ -8,13 +10,37 @@ router.get('/', (req, res) => {
 
 
 router.post('/signup',(req, res) => {
-    const {name, email, password} = req.body
+    const {name, email, password} = req.body;
 
     if(!email || !password || !name) {
         return res.status(422).json({error: "please add all the fields"})
     }
 
-    res.json({message: "successfully posted"})
+    User.findOne({email: email})
+        .then((savedUser) => {
+            if(savedUser) {
+                return res.status(422).json({error: "User already exists with that email address."})
+            }
+
+            const user = new User({
+                email, 
+                password,
+                name
+            })
+
+            user.save()
+            .then(user => {
+                res.json({message: "saved successfully"})
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+   
 });
 
 module.exports = router;

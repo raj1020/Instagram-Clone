afd342443794689d10edd52dbe2735bfc1f6d6f4
raj1020 <raj.1020@hotmail.com>
2020-08-28 09:18:30 +1000

    started to generate web token to enable the user to access protected resources

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model("User");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = require('../keys');
 
 
 router.get('/', (req, res) => {
@@ -68,7 +70,11 @@ router.post('/signin', (req, res) => {
         bcrypt.compare(password, savedUser.password)
         .then(doMatch => {
             if(doMatch) {
-                return res.json({message: "Successfully signed in"});
+                //return res.json({message: "Successfully signed in"});
+                const token = jwt.sign({_id: savedUser._id}, JWT_SECRET);
+                res.json({token})
+                //in the line above the key and the value ({token: token}) are both "token", hence it can just be written as a single {token}.
+
             } else {
                 return res.status(422).json({error: "Invalid Email or password"})
             }

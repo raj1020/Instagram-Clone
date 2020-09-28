81bf07f48fc6a3fc6a2ca689f65bdadd34af8545
raj1020 <raj.1020@hotmail.com>
2020-09-28 13:20:49 +1000

    defined the route and model for comments

--- models/post.js
+++ models/post.js
@@ -18,6 +18,11 @@ const postSchema = new mongoose.Schema({
         type: ObjectId,
         ref: "User"
     }],
+    comments: [{
+        text: String,
+        postedBy: {type: ObjectId, ref: "User"}
+
+    }],
     postedBy: {
         type: ObjectId,
         ref: "User"

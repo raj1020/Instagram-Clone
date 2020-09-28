4ef3f937ce27be890c5200bf06b29e01ab1e1e00
raj1020 <raj.1020@hotmail.com>
2020-09-28 14:26:51 +1000

    made corrections in allposts and comment routes to populate with more fields

--- routes/post.js
+++ routes/post.js
@@ -8,6 +8,7 @@ router.get('/allposts', requireLogin, (req, res) => {
 
     Post.find()
     .populate("postedBy","_id name")
+    .populate("comments.postedBy", "_id name")
     .then(posts => {
         res.json({posts})
     })
@@ -99,6 +100,7 @@ router.put('/comment', requireLogin, (req, res) => {
     }, {
         new: true
     }).populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
     .exec((err, result) => {
         if(err) {
             return res.status(422).json({error:err})
